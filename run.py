7c002598c6770a4bb0d31563a0ba55d261222c7d
Maxime Petazzoni <max@signalfuse.com>
2013-11-18 14:05:09 -0800

    Code cleanups
    Signed-off-by: Maxime Petazzoni <max@signalfuse.com>

--- run.py
+++ run.py
@@ -23,17 +23,17 @@ ELASTICSEARCH_CONFIG_FILE = 'config/elasticsearch.yml'
 # Get container/instance name.
 CONTAINER_NAME = os.environ.get('CONTAINER_NAME', '')
 assert CONTAINER_NAME, 'Container name is missing!'
-ELASTICSEARCH_CONFIG_BASE = re.sub(r'[^\w]', '_', CONTAINER_NAME).upper()
+CONFIG_BASE = re.sub(r'[^\w]', '_', CONTAINER_NAME).upper()
 
 # Get container's host IP address/hostname.
 CONTAINER_HOST_ADDRESS = os.environ.get('CONTAINER_HOST_ADDRESS', '')
 assert CONTAINER_HOST_ADDRESS, 'Container host address is required for ElasticSearch discovery!'
 
 # Gather configuration settings from environment.
-ELASTICSEARCH_CONFIG_CLUSTER_NAME = os.environ.get('ELASTICSEARCH_CONFIG_CLUSTER_NAME', 'local-elasticsearch')
-ELASTICSEACRH_CONFIG_ZOOKEEPER_BASE = os.environ.get('ELASTICSEACRH_CONFIG_ZOOKEEPER_BASE', '/local/elasticsearch')
-ELASTICSEARCH_CONFIG_PEER_PORT = int(os.environ.get('ELASTICSEARCH_%s_PEER_PORT' % ELASTICSEARCH_CONFIG_BASE, 9300))
-ELASTICSEARCH_CONFIG_HTTP_PORT = int(os.environ.get('ELASTICSEARCH_%s_HTTP_PORT' % ELASTICSEARCH_CONFIG_BASE, 9200))
+ELASTICSEARCH_CLUSTER_NAME = os.environ.get('ELASTICSEARCH_CLUSTER_NAME', 'local-elasticsearch')
+ELASTICSEACRH_ZOOKEEPER_BASE = os.environ.get('ELASTICSEACRH_ZOOKEEPER_BASE', '/local/elasticsearch')
+ELASTICSEARCH_PEER_PORT = int(os.environ.get('ELASTICSEARCH_{}_PEER_PORT'.format(CONFIG_BASE), 9300))
+ELASTICSEARCH_HTTP_PORT = int(os.environ.get('ELASTICSEARCH_{}_HTTP_PORT'.format(CONFIG_BASE), 9200))
 
 # Build ZooKeeper node list with host and client port for each node.
 ZOOKEEPER_NODE_LIST = []
@@ -47,11 +47,11 @@ assert ZOOKEEPER_NODE_LIST, 'ZooKeeper nodes are required for ElasticSearch disc
 # Prepare the YAML configuration and write it.
 with open(ELASTICSEARCH_CONFIG_FILE, 'w+') as conf:
     yaml.dump({
-        'cluster.name': ELASTICSEARCH_CONFIG_CLUSTER_NAME,
+        'cluster.name': ELASTICSEARCH_CLUSTER_NAME,
         'node.name': CONTAINER_NAME,
         'path.data': '/var/lib/elasticsearch',
-        'transport.tcp.port': ELASTICSEARCH_CONFIG_PEER_PORT,
-        'http.port': ELASTICSEARCH_CONFIG_HTTP_PORT,
+        'transport.tcp.port': ELASTICSEARCH_PEER_PORT,
+        'http.port': ELASTICSEARCH_HTTP_PORT,
         'network.publish_host': CONTAINER_HOST_ADDRESS,
         'discovery': {
                 'type': 'com.sonian.elasticsearch.zookeeper.discovery.ZooKeeperDiscoveryModule',
@@ -62,7 +62,7 @@ with open(ELASTICSEARCH_CONFIG_FILE, 'w+') as conf:
                 'client.host': ','.join(ZOOKEEPER_NODE_LIST),
                 'discovery.state_publishing.enabled': True,
         },
-        'zookeeper.root': ELASTICSEACRH_CONFIG_ZOOKEEPER_BASE,
+        'zookeeper.root': ELASTICSEACRH_ZOOKEEPER_BASE,
     }, conf, default_flow_style=False)
 
 # Start ElasticSearch
